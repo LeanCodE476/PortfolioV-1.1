f51402dca314bdd53f5406beb60967b51a1c6d40
Leandro Balmaceda <leandro20.balmaceda@gmail.com>
2022-04-28 10:57:10 -0300

    Prueba subir portfolio

---
+++ app.js
@@ -0,0 +1,25 @@
+const btnBurguer = document.querySelector(".panel-btn"),
+    menu = document.querySelector(".container-header");
+
+btnBurguer.addEventListener("click", () => {
+        menu.classList.toggle("is-active-efect");
+        btnBurguer.classList.toggle("is-active");
+    })
+window.sr = ScrollReveal();
+sr.reveal('.container-contact-footer', {
+    duration: 2000,
+});
+sr.reveal('.container-skills', {
+    duration: 1200,
+   
+});
+sr.reveal('.parrafo-about-me', {
+    duration: 1100,
+    origin: 'top',
+    distance:'120px'
+});
+sr.reveal('.briefcase-section', {
+    duration: 1600,
+});
+
+
